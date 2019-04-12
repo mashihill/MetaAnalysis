a4bba0f077b9338505a06eb825bd2c57451b547e
mashihill <keeneking@gmail.com>
2019-04-12 13:59:31 -0400

    minor change

--- proj597.R
+++ proj597.R
@@ -2,14 +2,16 @@ set.seed(123)
 p <- 5
 n1 <- 20
 n2 <- 15
+n3 = 30
 data1 <- data.frame(group=sample(1:3,n1,replace=TRUE), matrix(rnorm(p*n1),ncol=p))
 data2 <- data.frame(group=sample(1:2,n2,replace=TRUE), matrix(rnorm(p*n2),ncol=p))
+data3 <- data.frame(group=sample(1:3,n3,replace=TRUE), matrix(rnorm(p*n3),ncol=p))
+data.list = list(data1, data2, data3)
 
 library(dplyr)
 library(JumpTest)
 
-
-num.data = 2
+num.data = length(data.list)
 
 p.matrix = matrix(NA, nrow = num.data, ncol = p)
 pooled.p.matrix = data.frame(matrix(NA, nrow = 4, ncol = p))
@@ -31,36 +33,24 @@ stouffer.pool = function(p.vals) {
   k = length(p.vals)
   #print(sumz(p.vals))
   T_stouffer = sum(qnorm(p.vals)/sqrt(k))
-  #cat("T: ", T_stouffer, "\n")
-  #cat("builtin: ", sumz(p.vals)$p, "\n")
   pooled.p = pnorm(T_stouffer, lower.tail = T)
-  #cat("own: ", pooled.p, "\n")
   return(pooled.p)
 }
 
 min.pool = function(p.vals) {
   k = length(p.vals)
-  #print(sumz(p.vals))
   T_min = min(p.vals)
-  #cat("T: ", T_stouffer, "\n")
-  #cat("builtin: ", sumz(p.vals)$p, "\n")
   pooled.p = pbeta(T_min, 1, k)
-  #cat("own: ", pooled.p, "\n")
   return(pooled.p)
 }
 
 
 max.pool = function(p.vals) {
   k = length(p.vals)
-  #print(sumz(p.vals))
   T_max = max(p.vals)
-  #cat("T: ", T_stouffer, "\n")
-  #cat("builtin: ", sumz(p.vals)$p, "\n")
   pooled.p = pbeta(T_max, k, 1)
-  #cat("own: ", pooled.p, "\n")
   return(pooled.p)
 }
-
 
 test.performed <- c()
 
@@ -126,13 +116,12 @@ my.aov = function(data) {
 }
 
 
+
 ## Main
-p.matrix[1,] = my.aov(data1)$p.value.vec
-test.performed = c(test.performed, my.aov(data1)$test.performed)
-p.matrix[2,] = my.aov(data2)$p.value.vec
-test.performed = c(test.performed, my.aov(data2)$test.performed)
-
-
+for (i in 1:length(data.list)) {
+  p.matrix[i,] = my.aov(data.list[[i]])$p.value.vec
+  test.performed = c(test.performed, my.aov(data.list[[i]])$test.performed)
+}
 
 for (i in 1:dim(p.matrix)[2]) {
   pooled.p.matrix["Fisher", i] = fisher.pool(p.matrix[,i])
@@ -146,7 +135,7 @@ p.matrix
 pooled.p.matrix
 
 unique(test.performed)
-#
+
 #rbind(ppool(t(p.matrix), method = "FI")@pvalue,
 #      ppool(t(p.matrix), method = "SI")@pvalue,
 #      ppool(t(p.matrix), method = "MI")@pvalue,
