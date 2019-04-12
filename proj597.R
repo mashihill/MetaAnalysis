c0041794059fb774f8ab1452a720970b493f715e
Yen-An Chen <keene@Yen-Ans-MacBook-Pro.local>
2019-04-12 11:30:38 -0400

    Add #group==2 case

--- proj597.R
+++ proj597.R
@@ -1,9 +1,12 @@
 set.seed(123)
-p <- 6
-data1 <- data.frame(group=sample(1:3,200,replace=TRUE), matrix(rnorm(p*200),ncol=p))
-data2 <- data.frame(group=sample(1:2,150,replace=TRUE), matrix(rnorm(p*150),ncol=p))
+p <- 5
+n1 <- 20
+n2 <- 15
+data1 <- data.frame(group=sample(1:3,n1,replace=TRUE), matrix(rnorm(p*n1),ncol=p))
+data2 <- data.frame(group=sample(1:2,n2,replace=TRUE), matrix(rnorm(p*n2),ncol=p))
 
 library(dplyr)
+library(JumpTest)
 
 num.data = 2
 
@@ -58,37 +61,75 @@ max.pool = function(p.vals) {
 }
 
 
+test.performed <- c()
+
 my.aov = function(data) {
   p.value.vec = rep(NA, p)
   for (k in 1:p) {
-    listids <- list()
+    shapiro.vec = c()
     bioname = paste("X", k, sep = '')
     for (ids in unique(data$group)){
       subdf <- subset(x=data, subset=group==ids)
-      listids[[ids]] <- shapiro.test(get(bioname, subdf))
+      shapiro.vec <- c(shapiro.vec, shapiro.test(get(bioname, subdf))$p.value)
     }
     
     isNormal = T
-    for (i in 1:length(listids)) {
-        if (listids[[i]]$p.value < 0.05) {
-          isNormal = F
-        }
+    if (min(shapiro.vec) < 0.05) {
+      isNormal = F
     }
     
-    if (isNormal == F) {
-      res.aov = kruskal.test(as.formula(paste(bioname, "~ factor(group)")), data = data)
-      p.val = res.aov$p.value
-    } else {
-      res.aov = aov(as.formula(paste(bioname, "~ factor(group)")), data = data)
-      p.val = summary(res.aov)[[1]][["Pr(>F)"]][[1]]
-    }
+    if (length(unique(data$group)) > 2) {
+      if (isNormal == F) {
+        print('Kruskal')
+        test.performed = c(test.performed, 'Kruskal')
+        
+        res.aov = kruskal.test(as.formula(paste(bioname, "~ factor(group)")), data = data)
+        p.val = res.aov$p.value
+      } else {
+        print('Anova')
+        test.performed = c(test.performed, 'Anova')
+        print(test.performed)
+        res.aov = aov(as.formula(paste(bioname, "~ factor(group)")), data = data)
+        p.val = summary(res.aov)[[1]][["Pr(>F)"]][[1]]
+      }
+      
+    
+    } else { ##  #group == 2
+      subdf1 <- subset(x=data, subset=group==unique(data$group[1]))
+      subdf2 <- subset(x=data, subset=group==unique(data$group[2]))
+      
+      if (isNormal == T) {
+        fp <- var.test(get(bioname, subdf1), get(bioname, subdf2))$p.value
+        
+        if(fp>0.05) {  #equal variance
+          print('Equal T')
+          test.performed = c(test.performed, 'Equal T')
+          p.val <- t.test(get(bioname, subdf1),get(bioname, subdf2),var.equal=T)$p.value
+        } else {
+          print('Unequal T')
+          test.performed = c(test.performed, 'Unequal T')
+          p.val <- t.test(get(bioname, subdf1),get(bioname, subdf2),var.equal=F)$p.value
+        }
+      
+      } else {
+        print('Wilcox')
+        test.performed = c(test.performed, 'Wilcox')
+        p.val <- wilcox.test(get(bioname, subdf1),get(bioname, subdf2))$p.value
+      }
+
+      }
     p.value.vec[k] = p.val
   }
-  return(p.value.vec)
+  return(list(p.value.vec=p.value.vec, test.performed=test.performed))
+  
 }
 
-p.matrix[1,] = my.aov(data1)
-p.matrix[2,] = my.aov(data2)
+
+## Main
+p.matrix[1,] = my.aov(data1)$p.value.vec
+test.performed = c(test.performed, my.aov(data1)$test.performed)
+p.matrix[2,] = my.aov(data2)$p.value.vec
+test.performed = c(test.performed, my.aov(data2)$test.performed)
 
 
 
@@ -99,9 +140,13 @@ for (i in 1:dim(p.matrix)[2]) {
   pooled.p.matrix["maxP", i] = max.pool(p.matrix[,i])
 }
 
+
+p.matrix
 pooled.p.matrix
 
-rbind(ppool(t(p.matrix), method = "FI")@pvalue,
-      ppool(t(p.matrix), method = "SI")@pvalue,
-      ppool(t(p.matrix), method = "MI")@pvalue,
-      ppool(t(p.matrix), method = "MA")@pvalue)
+unique(test.performed)
+#
+#rbind(ppool(t(p.matrix), method = "FI")@pvalue,
+#      ppool(t(p.matrix), method = "SI")@pvalue,
+#      ppool(t(p.matrix), method = "MI")@pvalue,
+#      ppool(t(p.matrix), method = "MA")@pvalue)
